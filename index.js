57c7bdd57b096d0bf05496748c9573d9e50b101d
Stephen <slulego@live.com>
2018-06-17 19:02:19 -0400

    Fix sass and typescript throwing errors when not installed

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
+var path = require('path');
+
 var file = require('./file');
 var parser = require('./parser');
 
-var path = require('path');
-
 var options = require('./default.json');
 
-var sass = require('node-sass');
-var typescript = require('typescript');
+try {
+    var sass = require('node-sass');
+} catch (error) {
+    // No sass
+}
+try {
+    var typescript = require('typescript');
+} catch (error) {
+    // No typescript
+}
 
 // var beautify = require('js-beautify').html;
 
@@ -31,10 +39,7 @@ function compile(source, destination) {
     source = source.replace(/[\\\/]$/, '');
     destination = destination.replace(/[\\\/]$/, '');
 
-    // var destinationFolder = destination.match(/(\w*)[\\\/]/)[0];
-    // compileFile(source, path.dirname(source.replace(/(\w*)[\\\/]/, destinationFolder)) + '/');
     compileFile(source, destination);
-    // console.log(path.dirname(source.replace(/(\w*)[\\\/]/, destinationFolder)));
 }
 
 // Compile files
@@ -43,50 +48,32 @@ function compileFile(source, destination) {
     file.makeFolder(destination);
 
     if (file.isFolder(source)) {
-
-        // file.makeFolder(destination);
-        // file.copy(source, destination + source);
-
-        // file.makeFolder(destination);
         var files = file.list(source);
 
         for (const key in files) {
-            // console.log(source + '/' + files[key] + ' ' + destination);
-            // var destinationFolder = destination.match(/(\w*)[\\\/]/)[0];
-            // compileFile(source + files[key], path.dirname(source.replace(/(\w*)[\\\/]/, destinationFolder)));
-            // console.log(destinationFolder);
             compileFile(source + '/' + files[key], destination);
         }
     } else {
-        // console.log(source + ' ' + destination);
         var destinationFolder = path.dirname(source.replace(/^[\w-]*/, destination)) + '/';
         file.makeFolder(destinationFolder);
 
-        // console.log(destinationFolder);
         switch (path.extname(source)) {
             case '.ph':
                 var page = file.read(source);
                 try {
                     php = parser(page);
 
-                    // page = page.replace(/^\s*[\n\r]/g, ''); // Replace empty lines
-
-                    // page = page.replace(/\?>\s*\n*\s*<\?php /g, ''); // Remove double php tags
-
                     // page = beautify(page, {
                     //     extra_liners: '',
                     //     preserve_newlines: false
                     // });
-
-                    // php = php.replace(/<if (.*)>/, '<?php if($1): ?>');
-                    // php = php.replace('</if>', '<?php endif; ?>');
 
                     file.write(destinationFolder + path.parse(source).name + '.php', php);
                 } catch (error) {
                     var portion = page.substring(0, error.position);
                     var line = portion.split('\n').length - 1;
 
-                    // Debug
+                    // Pass javascript error
                     console.log(error);
 
                     showError('Phazor', error.code, source + ':' + line + ':' + error.position);
@@ -105,10 +92,10 @@ function compileFile(source, destination) {
                             file.write(destinationFolder + path.parse(source).name + '.js', output.outputText);
                         }
                     } catch (error) {
-                        showError('TypeScript', error.message, source + ':' + error.line);
+                        showError('TypeScript Error', error.message, source + ':' + error.line);
                     }
                 } else {
-                    showError('Phazor', 'The typescript package must be installed to compile .ts files.', false);
+                    showError('Warning', 'The \'typescript\' package must be installed to compile .ts files.', false);
                 }
                 break;
             case '.scss':
@@ -117,8 +104,8 @@ function compileFile(source, destination) {
                         var scss = file.read(source);
                         // Minify CSS if enabled in options
                         var minify = 'nested';
-                        if(options){
-                            if(options.minifyCSS == true){
+                        if (options) {
+                            if (options.minifyCSS == true) {
                                 minify = 'compressed';
                             }
                         }
@@ -130,10 +117,10 @@ function compileFile(source, destination) {
                             file.write(destinationFolder + path.parse(source).name + '.css', output.css);
                         }
                     } catch (error) {
-                        showError('Sass', error.message, source + ':' + error.line);
+                        showError('Sass Error', error.message, source + ':' + error.line);
                     }
                 } else {
-                    showError('Phazor', 'The node-sass package must be installed to compile .scss files.', false);
+                    showError('Warning', 'The \'node-sass\' package must be installed to compile .scss files.', false);
 
                 }
                 break;
@@ -147,15 +134,19 @@ function compileFile(source, destination) {
 function showError(type, message, location) {
     var color = '0';
     switch (type.toLowerCase()) {
-        case 'phazor':
+        case 'error':
+            // Red
+            color = '1;31';
+            break;
+        case 'warning':
+            // Yellow
+            color = '1;33';
+            break;
+        case 'typescript error':
             // Blue
             color = '1;34';
             break;
-        case 'typescript':
-            // Purple
-            color = '1;33';
-            break;
-        case 'sass':
+        case 'sass error':
             // Purple
             color = '1;35';
             break;
@@ -165,5 +156,5 @@ function showError(type, message, location) {
     } else {
         location = '';
     }
-    console.log('\x1b[' + color + 'm%s\x1b[0m', type + ' Error:', message, location);
+    console.log('\x1b[' + color + 'm%s\x1b[0m', type + ':', message, location);
 }
